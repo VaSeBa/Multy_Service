28c7823dd605f3118fa3d9321adfd3cadf485f61
VaSeBa <badasin.valentin@yandex.ru>
2021-10-23 22:55:13 +0300

    6_3_HW5_add_PayloadProcessor

--- sql/lb_apply.bat
+++ sql/lb_apply.bat
@@ -1,9 +1,9 @@
 #!/bin/bash
 set LB_HOME=/run/media/vaseba/HDD/Свободное_пространство/Liquibase/liquibase-4.5.0/
 call %LB_HOME%\liquibase.bat --driver=org.postgresql.Driver ^
---classpath=%LB_HOME%\lib ^
---changeLogFile=databaseChangeLog.sql ^
---url="jdbc:postgresql://localhost:5432/masterjava" ^
---username=user ^
---password=password ^
-migrate
+    --classpath=%LB_HOME%\lib ^
+    --changeLogFile=databaseChangeLog.sql ^
+    --url="jdbc:postgresql://localhost:5432/masterjava" ^
+    --username=user ^
+    --password=password ^
+    migrate
