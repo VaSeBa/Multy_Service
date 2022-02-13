55a3b341e9306e753656fb0eecc645ad98b259e0
vaseba <badasin.valentin@yandex.ru>
2022-02-13 17:41:55 +0300

    5_6_typesafe_config

--- common/src/main/java/ru/javaops/masterjava/config/Configs.java
+++ common/src/main/java/ru/javaops/masterjava/config/Configs.java
@@ -3,10 +3,6 @@ package ru.javaops.masterjava.config;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
-/**
- * gkislin
- * 01.11.2016
- */
 public class Configs {
 
     public static Config getConfig(String resource) {
