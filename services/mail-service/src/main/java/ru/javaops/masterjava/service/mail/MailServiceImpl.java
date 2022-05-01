00366464ea49b425dcfba5d71885ff84a82ae2ce
vaseba <badasin.valentin@yandex.ru>
2022-05-01 10:02:37 +0500

    7_10_send_bulk

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.javaops.masterjava.service.mail;
 
+import java.util.Set;
 import javax.jws.WebService;
 import java.util.List;
 
@@ -8,7 +9,17 @@ import java.util.List;
 )
 public class MailServiceImpl implements MailService {
 
-  public void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
-    MailSender.sendMail(to, cc, subject, body);
+//  public void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
+//    MailSender.sendMail(to, cc, subject, body);
+//  }
+
+  @Override
+  public String sendToGroup(Set<Addressee> to, Set<Addressee> cc, String subject, String body) {
+    return null;
+  }
+
+  @Override
+  public GroupResult sendBulk(Set<Addressee> to, String subject, String body) {
+    return null;
   }
 }
